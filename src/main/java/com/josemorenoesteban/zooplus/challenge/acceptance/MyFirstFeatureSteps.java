0ade541f63cab2081a3e2ad34b5162bd68976fbb
Jose Moreno <jomoespe@gmail.com>
2016-03-16 12:50:18 +0100

    Add a scenario with Then

--- src/main/java/com/josemorenoesteban/zooplus/challenge/acceptance/MyFirstFeatureSteps.java
+++ src/main/java/com/josemorenoesteban/zooplus/challenge/acceptance/MyFirstFeatureSteps.java
@@ -1,12 +1,24 @@
 package com.josemorenoesteban.zooplus.challenge.acceptance;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 
 public class MyFirstFeatureSteps {
+    private int cukes;
+    
     @Given("I have (\\d+) cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
-        System.out.format("The cukes are %s\n", cukes);
-        assert cukes == 24 : "Cikes are not 24";
+        assert cukes == 24 : "Cukes are not 24";
         //throw new cucumber.api.PendingException("Until this is not shwn it not finished!");
+    }
+
+    @Given("I put (\\d+) cukes in my belly")
+    public void I_put_cukes_in_my_belly(int cukes) {
+        this.cukes = cukes;
+    }
+    
+    @Then("I have more than (\\d+) cukes in my belly")
+    public void I_have_more_than_N_cukes_in_my_belly(int cukes) {
+        assert this.cukes > cukes : String.format("Cukes are less to equals than expected (result=%s, expected=%s)", cukes, this.cukes);
     }
 }
--- src/main/resources/com/josemorenoesteban/zooplus/challenge/acceptance/MyFirstFeature.feature
+++ src/main/resources/com/josemorenoesteban/zooplus/challenge/acceptance/MyFirstFeature.feature
@@ -5,3 +5,6 @@ Feature: My first feature
   Scenario: Some cukes
     Given I have 24 cukes in my belly
 
+  Scenario: More than 24 cukes
+    Given I put 25 cukes in my belly
+    Then I have more than 24 cukes in my belly
